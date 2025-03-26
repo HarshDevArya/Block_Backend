ec53d9eb4fbde1527918a6aa1c0a1f99dfdf63c4
HarshDevArya <harshdevarya96@gmail.com>
2025-03-26 11:54:32 +0530

    production

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://harsharyablog.netlify.app",
     credentials: true,
   })
 );
